decb3a495781140a23e1e58175f8ce647be2168f
Babul Akter <babulakterfsd@gmail.com>
2023-11-23 16:00:56 +0600

    all users fetching api implemented

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { userServices } from './user.service';
 import userValidation from './user.validation';
 
+// create user
 const createUser = async (req: Request, res: Response) => {
   try {
     const { user: userData } = req.body;
@@ -38,6 +39,29 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+// get all users
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.getAllUsersFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(404).json({
+      success: false,
+      message: 'Something went wrong while fetching users! Please try again.',
+      error: {
+        code: 404,
+        description: err.message,
+      },
+    });
+  }
+};
+
 export const UserControllers = {
   createUser,
+  getAllUsers,
 };
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,21 @@
 import { TUser } from './user.interface';
 import { UserModel } from './user.model';
 
+// create user in the database
 const createUserInTheDB = async (user: TUser) => {
   const result = await UserModel.create(user);
   return result;
 };
 
+// get all users from the database
+const getAllUsersFromDB = async () => {
+  const result = await UserModel.find().select(
+    'username fullName age email address -_id'
+  );
+  return result;
+};
+
 export const userServices = {
   createUserInTheDB,
+  getAllUsersFromDB,
 };
